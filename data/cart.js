6ba96c4a0090798638786a849262888832c27558
Emmadi Siddharth <emmadisiddharthreddy@gmail.com>
2025-06-11 19:30:34 +0530

    make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -3,10 +3,12 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart){
     cart = [{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:2
+        quantity:2,
+        deliveryOptionId: '1'
         },{
             productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-            quantity : 1
+            quantity : 1,
+            deliveryOptionId: '2'
         }];
 }
 
@@ -29,7 +31,8 @@ export function addtoCart(productId) {
     else {
         cart.push({
         productId: productId,
-        quantity : 1
+        quantity : 1,
+        deliveryOptionId: '1'
         });
 
     }
@@ -45,5 +48,28 @@ export function removeFromCart(productId){
         }
     });
     cart = newCart;
+    saveToStorage();
+}
+
+export function updateInCart(productId){
+    cart.forEach((cartItem) => {
+        if(cartItem.productId === productId){
+            cartItem.quantity+=1;
+        }
+    });
+    saveToStorage();
+}
+
+
+export function updateDeliveryOption(productId, deliveryOptionId){
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+    if(productId=== cartItem.productId){
+      matchingItem = cartItem;
+    }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
 }
