c98c7ebb83f153ad91b3e1b85a675fab00d256c9
Emmadi Siddharth <emmadisiddharthreddy@gmail.com>
2025-06-12 19:07:46 +0530

    Add jasmine testing framework

---
+++ tests/moneyTest.js
@@ -0,0 +1,9 @@
+import { formatCurrency } from '../scripts/utils/money.js'
+
+if(formatCurrency(2000.4) == '20.00'){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+console.log(formatCurrency(2000.4));
